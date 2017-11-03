82ce6472d70d19424e005967a3e32f3ea8478f25
Max Bulygin <maksym.bulygin@gmail.com>
2017-11-03 14:31:55 +0300

    started working with history api

--- js/application.js
+++ js/application.js
@@ -2,14 +2,46 @@ import welcome from './controller/welcome';
 import result from './controller/result';
 import level from './controller/level';
 
+const ControllerId = {
+  WELCOME: ``,
+  LEVEL: `level`,
+  RESULT: `result`
+};
+
+const saveGame = (data) => JSON.stringify(data);
+const loadGame = (data) => JSON.parse(data);
+
 export default class Application {
 
+  static init(state) {
+    Application.routes = {
+      [ControllerId.WELCOME]: welcome,
+      [ControllerId.LEVEL]: level(state),
+      [ControllerId.RESULT]: result
+    };
+
+    const hashChangeHandler = () => {
+      const hashValue = location.hash.replace(`#`, ``);
+      const [id, data] = hashValue.split(`?`);
+      Application.changeHash(id, data);
+    };
+    window.onhashchange = hashChangeHandler;
+    hashChangeHandler();
+  }
+
+  static changeHash(id, data) {
+    const controller = Application.routes[id];
+    if (controller) {
+      controller.init(loadGame(data));
+    }
+  }
+
   static showWelcome() {
-    welcome.init();
+    location.hash = ControllerId.WELCOME;
   }
 
   static showLevel(state) {
-    level.init(state);
+    location.hash = `${ControllerId.GAME}?${saveState(state)}`;
   }
 
   static showResult(game) {
