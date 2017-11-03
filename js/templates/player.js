372759030045093ab96c499b702b53c4badd40cd
Max Bulygin <maksym.bulygin@gmail.com>
2017-11-04 00:23:45 +0300

    no song overlapping feature added

--- js/templates/player.js
+++ js/templates/player.js
@@ -10,18 +10,33 @@ export const player = (audio) => `
   </div>`;
 
 export const bindPlayerEvents = (nodeList) => {
+  const disableOthers = (elements, current) => {
+    elements.forEach((it) => {
+      it.disabled = true;
+    });
+    current.disabled = false;
+  };
+  const enableAll = (elements) => {
+    elements.forEach((it) => {
+      it.disabled = false;
+    });
+  };
+  const buttons = [];
   nodeList.forEach((it) => {
     const track = it.querySelector(`audio`);
     const button = it.querySelector(`button`);
+    buttons.push(button);
     button.onclick = (evt) =>{
       const target = evt.target;
       evt.preventDefault();
       if (target.classList.contains(`player-control--pause`)) {
         target.classList.remove(`player-control--pause`);
         track.play();
+        disableOthers(buttons, target);
       } else {
         target.classList.add(`player-control--pause`);
         track.pause();
+        enableAll(buttons);
       }
     };
   });
