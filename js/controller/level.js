636948106213d5b588cce54888e36ce346e8fd81
Max Bulygin <maksym.bulygin@gmail.com>
2017-11-04 00:24:55 +0300

    removed unused import

--- js/controller/level.js
+++ js/controller/level.js
@@ -1,4 +1,4 @@
-import {processUserAnswer, initialState as initial, LEVELS, ARTIST_LEVEL, MISTAKES_ALLOWED} from "../data/game-data";
+import {processUserAnswer, LEVELS, ARTIST_LEVEL, MISTAKES_ALLOWED} from "../data/game-data";
 import {showScreen} from '../util';
 import App from '../application';
 import GenreView from '../view/genre-view';
