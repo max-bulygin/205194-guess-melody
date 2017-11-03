161bea1ed5e9c483629bf80ba072d018d9f00329
Max Bulygin <maksym.bulygin@gmail.com>
2017-11-03 22:38:17 +0300

    fixed bug of untracked mistakes in level.js

--- js/controller/level.js
+++ js/controller/level.js
@@ -1,4 +1,4 @@
-import {processUserAnswer, initialState as initial, LEVELS, ARTIST_LEVEL} from "../data/game-data";
+import {processUserAnswer, initialState as initial, LEVELS, ARTIST_LEVEL, MISTAKES_ALLOWED} from "../data/game-data";
 import {showScreen} from '../util';
 import App from '../application';
 import GenreView from '../view/genre-view';
@@ -9,7 +9,7 @@ class Level {
   init(game) {
     this.game = typeof game === `undefined` ? Object.assign({}, initial) : game;
     this.level = LEVELS[this.game.currentLevel];
-    if (!this.level) {
+    if (!this.level || this.game.mistakes > MISTAKES_ALLOWED) {
       App.showResult(this.game);
       return;
     }
