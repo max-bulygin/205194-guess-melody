061d74ba5b88ff17e6386168d27eb77110a42e46
Max Bulygin <maksym.bulygin@gmail.com>
2017-11-04 20:42:25 +0300

    trying to get result from server

--- js/controller/result.js
+++ js/controller/result.js
@@ -1,9 +1,11 @@
 import {showScreen} from '../util';
+import LoaderView from "../view/loader-view";
 import LossView from "../view/loss-view";
 import WinView from "../view/win-view";
 import App from '../application';
-import {SCREENS, MISTAKES_ALLOWED} from "../data/game-data";
+import {SCREENS, MISTAKES_ALLOWED, TIME_TOTAL, getScore} from "../data/game-data";
 import timer from '../timer';
+import Loader from '../loader';
 
 class Result {
   init(game) {
@@ -24,8 +26,24 @@ class Result {
       this.view = new LossView(SCREENS.attempts);
     }
     if (this.game.isComplete) {
-      this.view = new WinView(this.game);
+      Result.uploadResults({
+        time: TIME_TOTAL - this.game.time,
+        score: getScore(this.game.userAnswers)
+      });
+      const that = this;
+      Result.getLeaderBoard().
+          then((board) => {
+            that.view = new WinView(that.game, board);
+          });
     }
+  }
+
+  static uploadResults(data) {
+    Loader.saveResults(data);
+  }
+
+  static getLeaderBoard() {
+    return Loader.loadResults().then((result) => result.map((it) => it.score));
   }
 }
 
